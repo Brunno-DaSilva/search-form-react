0c3a5215fe11771f434647b5dc8ba51dbe106c16
Bruno DaSilva <hi.dasilvab@gmail.com>
2022-07-23 05:22:05 -0500

    added search component and search results on change

---
+++ src/components/ListPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ListPage = () => {
+  return (
+    <div>
+      <p>List results</p>
+    </div>
+  );
+};
+
+export default ListPage;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,11 +3,34 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
-const SearchBar = () => {
+const SearchBar = ({ posts, setSearchResults }) => {
+  const handleSubmit = (e) => e.preventDefault();
+
+  const handleSearchChange = (e) => {
+    if (!e.target.value) return setSearchResults(posts);
+
+    const resultsArray = posts.filter(
+      (post) =>
+        post.title.includes(e.target.value) ||
+        post.body.includes(e.target.value)
+    );
+    setSearchResults(resultsArray);
+  };
+
   return (
-    <div>
-      <h1>Search</h1>
-    </div>
+    <header>
+      <form className="search">
+        <input
+          className="search__input"
+          id="search"
+          type="text"
+          onChange={handleChange}
+        />
+        <button>
+          <FontAwesomeIcon icon={faMagnifyingGlass}></FontAwesomeIcon>
+        </button>
+      </form>
+    </header>
   );
 };
 
